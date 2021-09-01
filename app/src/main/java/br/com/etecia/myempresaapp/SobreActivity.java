13a65b8ea87c5e9bf65bec58d0654e3451b790d4
lndsilva <lndsilva@hotmail.com>
2021-09-01 19:44:43 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myempresaapp/SobreActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myempresaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SobreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sobre_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/myempresaapp/TermoUsoActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myempresaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermoUsoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.termo_uso_layout);
+    }
+}
