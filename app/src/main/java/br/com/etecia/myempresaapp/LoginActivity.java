5323e4b022bcc0b2f9cf6f4e7305447624fffc98
lndsilva <lndsilva@hotmail.com>
2021-09-08 20:20:23 -0300

    cores e manifest.

--- app/src/main/java/br/com/etecia/myempresaapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myempresaapp/LoginActivity.java
@@ -1,7 +1,6 @@
 package br.com.etecia.myempresaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
@@ -9,6 +8,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.login_layout);
+
     }
 }
